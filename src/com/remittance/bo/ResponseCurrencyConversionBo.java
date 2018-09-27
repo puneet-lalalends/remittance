cf739534ae7cc760b36f78c6f6008846735a2458
puneet <puneet@lalalends.com>
2018-09-27 10:39:24 +0530

    commit

---
+++ src/com/remittance/bo/ResponseCurrencyConversionBo.java
@@ -0,0 +1,14 @@
+package com.remittance.bo;
+
+public class ResponseCurrencyConversionBo {
+
+    private String currencyRate;
+
+    public String getCurrencyRate() {
+        return currencyRate;
+    }
+
+    public void setCurrencyRate(String currencyRate) {
+        this.currencyRate = currencyRate;
+    }
+}
